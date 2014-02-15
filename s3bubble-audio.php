f8a1ea9751c40d644f73b3ab60495921d62488a3
s3bubble <s3bubble@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-15 11:47:47 +0000

    Updates do deal with Caching plugins Fixes for responsive video player New style New video aspect ratio choice now choose between, 270p, 360p, Responsive
    git-svn-id: https://plugins.svn.wordpress.org/s3bubble-amazon-s3-audio-streaming/trunk@858392 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- s3bubble-audio.php
+++ s3bubble-audio.php
@@ -3,7 +3,7 @@
 Plugin Name: S3bubble Cloud Media Streaming Amazon S3
 Plugin URI: https://www.s3bubble.com/
 Description: S3Bubble offers simple, secure media streaming from Amazon S3 to WordPress. In just 3 simple steps. 
-Version: 1.6.4
+Version: 1.6.5
 Author: S3Bubble
 Author URI: https://s3bubble.com/
 License: GPL2
